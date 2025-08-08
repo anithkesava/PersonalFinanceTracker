525b5b2144d0b5bdc29ba6d346f29c20aaa00e54
anith kesava <anithkesava@gmail.com>
2025-08-08 08:37:22 +0530

    save changes

--- newuserscript.js
+++ newuserscript.js
@@ -1,13 +1,14 @@
-
 function closepopup() {
-    const profilepicturepopup = document.querySelector('.profilepicture-upload');
-    profilepicturepopup.style.display = 'none';
+  const profilepicturepopup = document.querySelector(".profilepicture-upload");
+  profilepicturepopup.style.display = "none";
 }
 
 function openpopup() {
-    const profilepicturepopup = document.querySelector('.profilepicture-upload');
-    profilepicturepopup.style.display = 'flex';    
+  const profilepicturepopup = document.querySelector(".profilepicture-upload");
+  profilepicturepopup.style.display = "flex";
 }
+
+
 function saveimg() {
-    alert('the value :' +document.getElementById('fileinput').value);
+ 
 }
