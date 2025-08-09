1e8b12d694539f7690e51e40570f59bf04ae7355
anith kesava <anithkesava@gmail.com>
2025-08-09 23:21:00 +0530

    save until create new user final

--- newuserscript.js
+++ newuserscript.js
@@ -48,7 +48,7 @@ function saveimg() {
       reader.readAsDataURL(file);
     }
   } else {
-      alert('no image has selected');
+    alert("no image has selected");
   }
 }
 
@@ -70,6 +70,100 @@ window.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-document.getElementById("savedetails").addEventListener("click", function () {
-  alert(screen.width);
+function validateAllInputs() {
+  const username = document.getElementById("nameinputid").value;
+  const emailinput = document.getElementById("mailinput").value;
+  const password = document.getElementById("passwordid").value;
+  const againpassword = document.getElementById("againpasswordid").value;
+
+  if (!emailinput.trim().endsWith("@gmail.com")) {
+    alert("email is invalid");
+    return;
+  }
+  if (password !== againpassword) {
+    alert("password should be same");
+    return;
+  }
+
+  if (username && emailinput && againpassword) {
+    localStorage.setItem("username", username);
+    localStorage.setItem("useremail", emailinput);
+    localStorage.setItem("password", againpassword);
+    const accountcreated = document.querySelector(
+      ".create-useracount-container"
+    );
+    accountcreated.style.display = "flex";
+  }
+}
+
+document.querySelector(".okay-btn").addEventListener("click", function () {
+  location.href = "./index.html";
 });
+
+document
+  .querySelector(".inputs-and-button")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (isUserExists()) {
+      alert("username or useremail already exists");
+      window.onload();
+      return;
+    }
+    validateAllInputs();
+  });
+
+function isUserExists() {
+  const username = document.getElementById("nameinputid").value;
+  const useremail = document.getElementById("mailinput").value;
+
+  const name = localStorage.getItem("username");
+  const email = localStorage.getItem("useremail");
+
+  if (username === name || useremail === email) {
+    return true;
+  }
+  return false;
+}
+
+document
+  .querySelector(".hamburger-icon")
+  .addEventListener("click", function () {
+    const sidebar = document.querySelector(".side-bar");
+    sidebar.style.display = "flex";
+  });
+
+document.querySelector(".close-icon").addEventListener("click", function () {
+  const sidebar = document.querySelector(".side-bar");
+  sidebar.style.display = "none";
+});
+document
+  .getElementById("RemovePicturelink")
+  .addEventListener("click", function () {
+    if (isprofileExists()) {
+      const removeprofilepicture = document.querySelector(
+        ".profile-picture-delete-container"
+      );
+      removeprofilepicture.style.display = "flex";
+      return;
+    }
+    alert("no profile picture exist to remove");
+  });
+
+function isprofileExists() {
+  const profilepicture = localStorage.getItem("profilepicture");
+  if (profilepicture) {
+    return true;
+  }
+  return false;
+}
+
+document.querySelector(".ok-icon").addEventListener("click", function () {
+  localStorage.removeItem('profilepicture');
+  window.onload;
+
+});
+
+document.querySelector('.cancel-icon').addEventListener('click', function () {
+  const removeprofilepopup = document.querySelector('.profile-picture-delete-container');
+  removeprofilepopup.style.display = 'none';
+})
