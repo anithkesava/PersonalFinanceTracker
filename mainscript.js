ce0a1aafa8d9dd5eb3f79b4df3ef39b303e862df
anith kesava <anithkesava@gmail.com>
2025-08-14 07:31:31 +0530

    save untill userdetails

--- mainscript.js
+++ mainscript.js
@@ -1,15 +1,13 @@
 window.addEventListener("DOMContentLoaded", function () {
   const header = document.getElementById("header");
   const username = localStorage.getItem("username");
-
   const h1 = document.createElement("h1");
   const strong = this.document.createElement("strong");
   h1.textContent = "Hello, ";
   strong.textContent = username;
   header.appendChild(h1);
   header.appendChild(strong);
-
-  const profile = document.getElementById("profile-picture");
+  // const profile = document.getElementById("profile-picture");
   const profilepicture = localStorage.getItem("profilepicture");
   if (!profilepicture) {
     const imgcontainer = document.querySelector(".defaulticon");
@@ -20,7 +18,9 @@ window.addEventListener("DOMContentLoaded", function () {
     const img = document.getElementById("profile-picture");
     img.src = profilepicture;
   }
-
+  const today = new Date().toISOString().split("T")[0];
+  const dateinput = document.getElementById("dob");
+  dateinput.setAttribute("max", today);
   function makeincomedropdown() {
     const select = document.getElementById("select");
     const primaryincomesource = [
@@ -46,7 +46,6 @@ window.addEventListener("DOMContentLoaded", function () {
   function prefilluserdetails() {
     const name = localStorage.getItem("username");
     const email = localStorage.getItem("useremail");
-
     if (name && email) {
       document.getElementById("username").value = name;
       document.getElementById("useremail").value = email;
@@ -119,6 +118,107 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   }
   makepaymentmethoddropdown();
+
+  const source = localStorage.getItem("primarysource");
+  const primarysourcedetails = localStorage.getItem(
+    "primarysource-information"
+  );
+  const userdetails = localStorage.getItem("userinfo");
+  if (
+    source !== null &&
+    primarysourcedetails !== null &&
+    userdetails !== null
+  ) {
+    document.querySelector(".savebtn").textContent = "close";
+  } else {
+    document.querySelector(".savebtn").textContent = "save";
+  }
+
+  //here we need to fill the all input fields if they already filled by the user.
+  const userinfo = localStorage.getItem("userinfo");
+  if (typeof userinfo === "string") {
+    //we need to retrieve the value and assign that to respective input fields.
+    const user = JSON.parse(userinfo);
+    document.getElementById("phone").value = user.mobile;
+    document.getElementById("dob").value = user.dateofbirth;
+    const primarysource = this.localStorage.getItem("primarysource");
+    if (primarysource !== null) {
+      document.getElementById("select").value = primarysource;
+      this.document.getElementById("select").disabled = true;
+    }
+  }
+  //now we need the dynamic box's content needs to be appear based on the primary income source.
+  const primarysource = this.localStorage.getItem("primarysource");
+  const employeecontainer = document.querySelector(
+    ".employee-is-a-primary-source"
+  );
+  const businesscontainer = document.querySelector(
+    ".business-is-a-primary-source"
+  );
+  const freelancecontainer = document.querySelector(
+    ".freelance-is-a-primary-source"
+  );
+  if (primarysource === "employee") {
+    employeecontainer.style.display = "flex";
+    businesscontainer.style.display = "none";
+    freelancecontainer.style.display = "none";
+
+    const employee = localStorage.getItem("primarysource-information");
+    const employeedetails = JSON.parse(employee);
+    document.getElementById("companyname").value = employeedetails.companyname;
+    document.getElementById("designation").value = employeedetails.role;
+    document.getElementById("ctc").value = employeedetails.ctc;
+    document.getElementById("montlysalary").value = employeedetails.salary;
+    document.getElementById("bonus").value = employeedetails.montlybonus;
+  } else if (primarysource === "business") {
+    employeecontainer.style.display = "none";
+    businesscontainer.style.display = "flex";
+    freelancecontainer.style.display = "none";
+
+    const business = this.localStorage.getItem("primarysource-information");
+    const businessdetails = JSON.parse(business);
+    document.getElementById("businessname").value =
+      businessdetails.businessname;
+    if (businessdetails.businesstype != null) {
+      document.getElementById("business-type").value =
+        businessdetails.businesstype;
+      document.getElementById("business-type").disabled = true;
+      document.getElementById("business-type").style.cursor = "not-allowed";
+    }
+    document.getElementById("industry").value = businessdetails.industryname;
+    document.getElementById("years").value = businessdetails.yearsofoperation;
+    document.getElementById("employeecount").value =
+      businessdetails.employeecount;
+    document.getElementById("business-monthly-income").value =
+      businessdetails.montlysalary;
+    if (businessdetails.primarymarket != null) {
+      document.getElementById("primary-market").value =
+        businessdetails.primarymarket;
+      document.getElementById("primary-market").disabled = true;
+      document.getElementById("primary-market").style.cursor = "not-allowed";
+    }
+  } else if (primarysource === "freelance") {
+    employeecontainer.style.display = "none";
+    businesscontainer.style.display = "none";
+    freelancecontainer.style.display = "flex";
+
+    const freelancedetails = localStorage.getItem("primarysource-information");
+    const freelance = JSON.parse(freelancedetails);
+
+    document.getElementById("freelanceservice").value =
+      freelance.freelanceservice;
+    document.getElementById("freelanceexperience").value =
+      freelance.freelanceexperience;
+    document.getElementById("freelancemontlysalary").value =
+      freelance.montlysalary;
+    document.getElementById("clientname").value = freelance.freelanceclient;
+    if (document.getElementById("payment-method").value !== null) {
+      document.getElementById("payment-method").value = freelance.paymentmethod;
+      document.getElementById("payment-method").disabled = true;
+      document.getElementById("payment-method").style.cursor = "not-allowed";
+    }
+    document.getElementById("working-plaftform").value = freelance.platform;
+  }
 });
 
 document
@@ -127,7 +227,6 @@ document
     const sidebar = document.querySelector(".side-bar");
     sidebar.classList.toggle("active");
   });
-
 document.getElementById("select").addEventListener("change", function () {
   const employeecontainer = document.querySelector(
     ".employee-is-a-primary-source"
@@ -138,55 +237,202 @@ document.getElementById("select").addEventListener("change", function () {
   const freelancecontainer = document.querySelector(
     ".freelance-is-a-primary-source"
   );
+  //required input fields.
+  const employeeinputs = document.querySelectorAll(".employee-input");
+  const businessinputs = document.querySelectorAll(".business-input");
+  const freelanceinputs = document.querySelectorAll(".freelance-input");
 
   if (this.value == "employee") {
     employeecontainer.style.display = "flex";
     businesscontainer.style.display = "none";
     freelancecontainer.style.display = "none";
+    //Employee Required Fields.
+    for (let inputs of employeeinputs) {
+      inputs.required = true;
+    }
+    businessinputs.forEach((x) => {
+      x.required = false;
+    });
+    freelanceinputs.forEach((x) => {
+      x.required = false;
+    });
   } else if (this.value == "business") {
     employeecontainer.style.display = "none";
     businesscontainer.style.display = "flex";
     freelancecontainer.style.display = "none";
+    //Business Required Fields
+    for (let inputs of employeeinputs) {
+      inputs.required = false;
+    }
+    businessinputs.forEach((x) => {
+      x.required = true;
+    });
+    freelanceinputs.forEach((x) => {
+      x.required = false;
+    });
   } else if (this.value == "freelance") {
     employeecontainer.style.display = "none";
     businesscontainer.style.display = "none";
     freelancecontainer.style.display = "flex";
+    //FreeLance Required Fields
+    for (let inputs of employeeinputs) {
+      inputs.required = false;
+    }
+    businessinputs.forEach((x) => {
+      x.required = false;
+    });
+    freelanceinputs.forEach((x) => {
+      x.required = true;
+    });
   } else {
     employeecontainer.style.display = "none";
     businesscontainer.style.display = "none";
     freelancecontainer.style.display = "none";
   }
 });
-
 document.getElementById("add-user-id").addEventListener("click", function () {
   const adduser = document.querySelector(".add-expense-income");
   const sidebar = document.querySelector(".side-bar");
   adduser.style.display = "flex";
-  sidebar.classList.toggle('active');
+  sidebar.classList.toggle("active");
 });
 document.getElementById("signout-id").addEventListener("click", function () {
   location.href = "./index.html";
 });
-document
-  .getElementById("business-type")
-  .addEventListener("change", function () {
-    alert(this.value);
-  });
 
-document
-  .getElementById("primary-market")
-  .addEventListener("change", function () {
-    alert(this.value);
-  });
-
-document
-  .getElementById("payment-method")
-  .addEventListener("change", function () {
-    alert(this.value);
-  });
-
+//save button function.
 document
   .getElementById("userinformation")
-  .addEventListener("submit", function () {
-    alert("form submitted");
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    const popup = document.querySelector(".add-expense-income");
+    const btn = document.querySelector(".savebtn");
+    if (btn.textContent.toLowerCase() == "close") {
+      popup.style.display = "none";
+      return;
+    }
+
+    const username = document.getElementById("username").value;
+    const useremail = document.getElementById("useremail").value;
+    console.log(username);
+    console.log(useremail);
+    const phone = document.getElementById("phone").value;
+    const dob = document.getElementById("dob").value;
+
+    const userinfo = {
+      name: username,
+      email: useremail,
+      mobile: phone,
+      dateofbirth: dob,
+    };
+
+    const primarysource = document.getElementById("select").value;
+    console.log("primary source: " + primarysource);
+
+    if (primarysource === "employee") {
+      const cname = document.getElementById("companyname").value;
+      const designation = document.getElementById("designation").value;
+      const ctc = document.getElementById("ctc").value;
+      const montlysalary = document.getElementById("montlysalary").value;
+      const bonus = document.getElementById("bonus").value;
+
+      let incomesourcedata = {
+        companyname: cname,
+        role: designation,
+        ctc: ctc,
+        salary: montlysalary,
+        montlybonus: bonus == "" ? 0 : bonus,
+      };
+      //make sure whether the primary of income is also selected.
+      const incomealreadyexists = localStorage.getItem(
+        "primarysource-information"
+      );
+      if (typeof incomealreadyexists === "string") {
+        alert("you already choose an primary income");
+        popup.style.display = "none";
+        return;
+      } else {
+        localStorage.setItem("primarysource", primarysource);
+        localStorage.setItem(
+          "primarysource-information",
+          JSON.stringify(incomesourcedata)
+        );
+        localStorage.setItem("userinfo", JSON.stringify(userinfo));
+        alert("employee details saved");
+        popup.style.display = "none";
+        return;
+      }
+    } else if (primarysource === "business") {
+      const bname = document.getElementById("businessname").value;
+      const btype = document.getElementById("business-type").value;
+      const industry = document.getElementById("industry").value;
+      const years = document.getElementById("years").value;
+      const ecount = document.getElementById("employeecount").value;
+      const salary = document.getElementById("business-monthly-income").value;
+      const market = document.getElementById("primary-market").value;
+
+      let incomesourcedata = {
+        businessname: bname,
+        businesstype: btype,
+        industryname: industry,
+        yearsofoperation: years,
+        employeecount: ecount,
+        montlysalary: salary,
+        primarymarket: market,
+      };
+      const primaryincomedetails = localStorage.getItem(
+        "primarysource-information"
+      );
+      if (typeof primaryincomedetails === "string") {
+        alert("you already choose an primary income");
+        popup.style.display = "none";
+        return;
+      } else {
+        localStorage.setItem("primarysource", primarysource);
+        localStorage.setItem(
+          "primarysource-information",
+          JSON.stringify(incomesourcedata)
+        );
+        localStorage.setItem("userinfo", JSON.stringify(userinfo));
+        alert("business details saved");
+        popup.style.display = "none";
+        return;
+      }
+    } else if (primarysource === "freelance") {
+      const service = document.getElementById("freelanceservice").value;
+      const experience = document.getElementById("freelanceexperience").value;
+      const salary = document.getElementById("freelancemontlysalary").value;
+      const client = document.getElementById("clientname").value;
+      const paymentmode = document.getElementById("payment-method").value;
+      const workingplatform =
+        document.getElementById("working-plaftform").value;
+
+      let incomesourcedata = {
+        freelanceservice: service,
+        freelanceexperience: experience,
+        montlysalary: salary,
+        freelanceclient: client,
+        paymentmethod: paymentmode,
+        platform: workingplatform,
+      };
+      const primaryincomedetails = localStorage.getItem(
+        "primarysource-information"
+      );
+      if (typeof primaryincomedetails === "string") {
+        alert("you already choose an primary income");
+        popup.style.display = "none";
+        return;
+      } else {
+        localStorage.setItem("primarysource", primarysource);
+        localStorage.setItem(
+          "primarysource-information",
+          JSON.stringify(incomesourcedata)
+        );
+        localStorage.setItem("userinfo", JSON.stringify(userinfo));
+        alert("freelance details saved");
+        popup.style.display = "none";
+        return;
+      }
+    } else {
+    }
   });
